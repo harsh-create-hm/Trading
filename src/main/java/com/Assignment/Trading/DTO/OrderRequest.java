a342681262332028a7dc7c50f58ad9dc6e091eac
Harshad <harshadmeshram98@gmail.com>
2026-03-28 23:53:41 +0530

    Refactor OrderRequest

--- src/main/java/com/Assignment/Trading/DTO/OrderRequest.java
+++ src/main/java/com/Assignment/Trading/DTO/OrderRequest.java
@@ -5,16 +5,10 @@ import com.Assignment.Trading.Enums.OrderSide;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 
-@Data
-@AllArgsConstructor
 public class OrderRequest {
 	
-	
-
 	@NotBlank
 	private String traderId;
 	
