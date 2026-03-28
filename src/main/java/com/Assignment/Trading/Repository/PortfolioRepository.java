b1c60e31f9b05df60246a005059c16c4cc69c4c1
Harshad <harshadmeshram98@gmail.com>
2026-03-29 00:16:48 +0530

    Refactored Code

--- src/main/java/com/Assignment/Trading/Repository/PortfolioRepository.java
+++ src/main/java/com/Assignment/Trading/Repository/PortfolioRepository.java
@@ -3,9 +3,10 @@ package com.Assignment.Trading.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.Assignment.Trading.Entity.Portfolio;
 
+@Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     Optional<Portfolio> findByTraderId(String traderId);
 }
