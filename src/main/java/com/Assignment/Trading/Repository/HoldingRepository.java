0c47cb6f8e68b268f48e1276d6ed339406891993
Harshad <harshadmeshram98@gmail.com>
2026-03-29 00:16:01 +0530

    Refactored Code

--- src/main/java/com/Assignment/Trading/Repository/HoldingRepository.java
+++ src/main/java/com/Assignment/Trading/Repository/HoldingRepository.java
@@ -2,7 +2,7 @@ package com.Assignment.Trading.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +11,7 @@ import com.Assignment.Trading.Entity.Holding;
 import com.Assignment.Trading.Entity.Portfolio;
 
 import jakarta.persistence.LockModeType;
-
+@Repository
 public interface HoldingRepository extends JpaRepository<Holding, Long> {
 
 	Optional<Holding> findByPortfolioAndStock(Portfolio portfolio,String Stock);
